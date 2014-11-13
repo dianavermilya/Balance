8aa759ca71fb9547dece667836b1539c5bc12669
Diana Vermilya <dianavermilya@gmail.com>
2014-11-13 16:47:31 -0500

    added groceryItems for all items

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,13 +3,18 @@ angular.module('starter.controllers', [])
 .controller('PreferencesCtrl', function($scope) {
 })
 
-.controller('GroceryListCtrl', function($scope, GroceryList) {
-  $scope.groceryList = GroceryList.all();
-})
+.controller('GroceryListCtrl', function($scope, GroceryList, GroceryItems) {
+  var groceryListIndexes = GroceryList.all();
+  var groceryItems = GroceryItems.all();
+  var groceryList = {};
+  for (var i=0;i<groceryListIndexes.length; i++){
+  	groceryList.push(groceryItems[groceryListIndexes]);
+  };
+});
 
 .controller('ItemDetailCtrl', function($scope, $stateParams, GroceryList) {
   $scope.item = GroceryList.get($stateParams.itemId);
-})
+});
 
-.controller('SearchCtrl', function($scope) {
+.controller('AccountCtrl', function($scope) {
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,27 +1,47 @@
 angular.module('starter.services', [])
 
-/**
- * A simple example service that returns some data.
- */
-.factory('GroceryList', function() {
+
+.factory('GroceryItems', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var groceryList = [
+  var groceryItems = [
     { id: 0, name: '1 dozen eggs' },
     { id: 1, name: '1 gal 2% milk' },
     { id: 2, name: '4 lbs potatoes' },
     { id: 3, name: '1 qt yogurt' },
-    { id: 4, name: '1 pt blueberries' }
+    { id: 4, name: '1 pt blueberries' },
+    { id: 5, name: 'ribeye steak' },
+    { id: 6, name: 'lean chicken breast' },
+    { id: 7, name: 'Peanut butter clif bar' },
+    { id: 8, name: 'Vanilla Protein Shake' },
+    { id: 9, name: 'Tofu' }
+
+  ];
+
+  return {
+    all: function() {
+      return groceryItems;
+    },
+    get: function(itemId) {
+      // Simple index lookup
+      return groceryItems[itemId];
+    }
+  }
+});
+
+
+.factory('GroceryList', function() {
+
+  var groceryList = [0,1,3,6,9];
   ];
 
   return {
     all: function() {
       return groceryList;
     },
-    get: function(itemId) {
-      // Simple index lookup
-      return groceryList[itemId];
+    get: function(index) {
+      return groceryList[index];
     }
   }
 });
