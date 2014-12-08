744298e9083404c258680f4f7b0b2795798b5129
Diana Vermilya <dianavermilya@gmail.com>
2014-12-07 22:31:05 -0500

    implemented constraints on search

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,6 +3,12 @@ angular.module('starter.controllers', [])
 .controller('PreferencesCtrl', function($scope, Constraints) {
 	var constraintsList = Constraints.all();
 	$scope.constraintsList = constraintsList;
+  $scope.updateConstraint = function (key) {
+    var constraint = Constraints.get(key);
+    constraint.selected = !constraint.selected;
+    Constraints.save(key, constraint);
+    $scope.constraintsList[key] = constraint;
+  };
 })
 
 .controller('GroceryListCtrl', function($scope, GroceryList, GroceryItems) {
@@ -22,7 +28,6 @@ angular.module('starter.controllers', [])
 .controller('SearchCtrl', function($scope, FoodGroupTree, NutritionTree, Constraints) {
 	$scope.foodGroupTree = FoodGroupTree.all();
 	$scope.nutritionTree = NutritionTree.all();
-	$scope.constraints = Constraints.all();
 })
 
 .controller('MidSearchCtrl', function($scope, $stateParams, FoodGroupTree, NutritionTree, Constraints) {
@@ -34,10 +39,29 @@ angular.module('starter.controllers', [])
   var groceryItems = GroceryItems.all();
   var groupId = $stateParams.group;
   var subGroupId = $stateParams.subGroup;
-  var selectGroceryIndexes = FoodGroupTree.all()[groupId].children[subGroupId].items
+  var selectGroceryIndexes = FoodGroupTree.all()[groupId].children[subGroupId].items;
+
   var selectGroceryItems = [];
-  for (var i=0;i<selectGroceryIndexes.length; i++){
-    selectGroceryItems.push(groceryItems[selectGroceryIndexes[i]]);
+  var constraints = Constraints.all();
+
+  function isAllowed(food) {
+    var allowed = true
+    food.constraintsViolated.every(function(violation) {
+      if (constraints[violation].selected) {
+        allowed = false;
+        return false;        
+      }
+      else return true;
+    });
+    return allowed;
+  }
+
+  var groceryItem;
+  for (var i=0;i<selectGroceryIndexes.length;i++){
+    groceryItem = groceryItems[selectGroceryIndexes[i]]
+    if (isAllowed(groceryItem)) {
+      selectGroceryItems.push(groceryItem);
+    }
   };
   $scope.selectGroceryItems = selectGroceryItems;
 });
