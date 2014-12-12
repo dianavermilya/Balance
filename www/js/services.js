c58cf0f1f84157e099bc711c02105f2b6c7a6fdc
ihoover <polonian@gmail.com>
2014-12-11 23:24:03 -0500

    added ingredients to detail view

--- www/js/services.js
+++ www/js/services.js
@@ -15,7 +15,7 @@ angular.module('starter.services', [])
     { id: 6, name: 'lean chicken breast' },
     { id: 7, name: 'Peanut butter clif bar' },
     { id: 8, name: 'Vanilla Protein Shake' },
-    { id: 9, name: 'Tofu', price: 5.89 },
+    { id: 9, name: 'Tofu', price: 5.89, ingredients: "Water, Organic Whole Soybeans, Calcium Sulfate, Tricalcium Phosphate, Nigari (Sea Water Extract), Vitamin B6, Riboflavin (Vitamin B2), Vitamin D2, Vitamin B12." },
     { id: 10, name: 'Muscle-Man Choco-Chug' },
     { id: 11, name: 'Power Powder' },
     { id: 12, name: 'Mocho-Man Munchy Bar' }
