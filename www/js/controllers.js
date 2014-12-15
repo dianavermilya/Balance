5cd06edfab07f6a6cd7943a5bd81eb4feb39b5bd
dianavermilya <dvermilya@onshape.com>
2014-12-14 23:00:55 -0500

    charts

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,7 +5,6 @@ angular.module('starter.controllers', [])
   var groceryItems = GroceryItems.all();
   var groceryList = [];
   var groceryItem;
-  var itemId;
   groceryListObjects.forEach(function (itemMetaData) {
     groceryItem = groceryItems[itemMetaData.id]
     console.log(groceryItem, itemMetaData)
@@ -91,12 +90,12 @@ angular.module('starter.controllers', [])
 })
 
 .controller('BalanceCtrl', function($scope, $stateParams, GroceryItems, GroceryList) {
-  
+  $scope.num = {};
+
   var groceryListObjects = GroceryList.get("list");
   var groceryItems = GroceryItems.all();
   var groceryList = [];
   var groceryItem;
-  var itemId;
   groceryListObjects.forEach(function (itemMetaData) {
     groceryItem = groceryItems[itemMetaData.id]
     console.log(groceryItem, itemMetaData)
@@ -104,25 +103,54 @@ angular.module('starter.controllers', [])
     groceryItem.checked = itemMetaData.checked;
     groceryList.push(groceryItem);
   });
-    
+
+  var protein_total = 0;
+  var sugar_total = 0;
+  var carbs_total = 0;
+  var fat_total = 0;
+
+  var quantity
+  groceryList.forEach(function(item) {
+    quantity = item.quantity
+    protein_total+=item.nutritionalData.protein*quantity
+    sugar_total+=item.nutritionalData.sugar*quantity
+    carbs_total+=item.nutritionalData.carbohydrate*quantity
+    fat_total+=item.nutritionalData.protein*quantity
+  });
+
   google.load("visualization", "1", {packages:["corechart"]});
 
-  drawProteinChart();
-  $scope.num_days = 1;
-  $scope.num_people = 1;
+  $scope.num.days = 2;
+  $scope.num.people = 7;
 
-  daily_values = {
-    protein: 50,
-    sugar: 30,
-    carbs: 125,
-    fat: 60
+  $scope.calculate_pie_values = function() {
+
+    var ideal_values = {
+      protein: 50*$scope.num.days*$scope.num.people,
+      sugar: 56*$scope.num.days*$scope.num.people,
+      carbs: 125*$scope.num.days*$scope.num.people,
+      fat: 60*$scope.num.days*$scope.num.people
+    }
+
+    $scope.protein_present = protein_total/ideal_values.protein*100;
+    $scope.protein_missing = Math.max(100 - $scope.protein_present, 0);
+    $scope.sugar_present = sugar_total/ideal_values.sugar*100;
+    $scope.sugar_missing = Math.max(100 - $scope.sugar_present, 0);
+    $scope.carbs_present = carbs_total/ideal_values.carbs*100;
+    $scope.carbs_missing = Math.max(100 - $scope.carbs_present, 0);
+    $scope.fat_present = fat_total/ideal_values.fat*100;
+    $scope.fat_missing = Math.max(100 - $scope.fat_present,0);
+    
+    drawCharts();
   }
+  $scope.calculate_pie_values()
+
   //google.setOnLoadCallback(drawChart);
   function drawProteinChart() {
     var data = google.visualization.arrayToDataTable([
       ['Nutrition THingy', 'Grams'],
-      ['Protein',     11],
-      ['Hidden',      2]
+      ['Protein', $scope.protein_present],
+      ['Hidden', $scope.protein_missing]
     ]);
 
     var options = {
@@ -132,13 +160,90 @@ angular.module('starter.controllers', [])
       pieStartAngle: 135,
       tooltip: { trigger: 'none' },
       slices: {
-        0: { color: 'blue' },
+        0: { color: '441053' },
         1: { color: 'transparent' }
       }
     };
     var chart = new google.visualization.PieChart(document.getElementById("piechart0"));
    chart.draw(data, options);
   }
+
+  function drawSugarChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Nutrition THingy', 'Grams'],
+      ['Protein', $scope.sugar_present],
+      ['Hidden', $scope.sugar_missing]
+    ]);
+
+    var options = {
+      title: 'Sugar',
+      legend: 'none',
+      pieSliceText: 'none',
+      pieStartAngle: 135,
+      tooltip: { trigger: 'none' },
+      slices: {
+        0: { color: '241757' },
+        1: { color: 'transparent' }
+      }
+    };
+    var chart = new google.visualization.PieChart(document.getElementById("piechart1"));
+   chart.draw(data, options);
+  }
+
+  function drawCarbsChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Nutrition THingy', 'Grams'],
+      ['Protein', $scope.carbs_present],
+      ['Hidden', $scope.carbs_missing]
+    ]);
+
+    var options = {
+      title: 'Carbohydrates',
+      legend: 'none',
+      pieSliceText: 'none',
+      pieStartAngle: 135,
+      tooltip: { trigger: 'none' },
+      slices: {
+        0: { color: '133351' },
+        1: { color: 'transparent' }
+      }
+    };
+    var chart = new google.visualization.PieChart(document.getElementById("piechart2"));
+   chart.draw(data, options);
+  }
+
+  function drawFatChart() {
+    console.log("faat", $scope.fat_present, $scope.fat_missing);
+
+
+    var data = google.visualization.arrayToDataTable([
+      ['Nutrition THingy', 'Grams'],
+      ['Protein', $scope.fat_present],
+      ['Hidden', $scope.fat_missing]
+    ]);
+
+    var options = {
+      title: 'Total Fat',
+      legend: 'none',
+      pieSliceText: 'none',
+      pieStartAngle: 135,
+      tooltip: { trigger: 'none' },
+      slices: {
+        0: { color: '3E3175' },
+        1: { color: 'transparent' }
+      }
+    };
+    var chart = new google.visualization.PieChart(document.getElementById("piechart3"));
+   chart.draw(data, options);
+  }
+
+  function drawCharts() {
+    drawProteinChart();
+    drawSugarChart();
+    drawCarbsChart();
+    drawFatChart();
+  }
+
 })
 
 
